6909a27b408169b8555e64f51dc79b3309a065ea
Oliver <ogodoys@miumg.edu.gt>
2026-09-08 20:14:27 -0600

    feat: barra de navegacion movil para abrir el menu lateral (SDGODA-52)

--- app/Views/layouts/sidenav.php
+++ app/Views/layouts/sidenav.php
@@ -1,3 +1,4 @@
+<?= view('layouts/navbar-movil') ?>
 <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3"
        id="sidenav-main">
 
